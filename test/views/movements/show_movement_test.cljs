24d9c751f3d7578dc26dd8ca8cc9d369f0846894
Mylab <labs@onmycrowd.com>
2020-04-04 18:18:06 -0700

    Refactoring test case

--- test/views/movements/show_movement_test.cljs
+++ test/views/movements/show_movement_test.cljs
@@ -12,10 +12,12 @@
      [:h4 "Bad input"]
      [:pre [:code (s/explain-str ::spec/movement m)]]]))
 
+(def movement {:id #uuid "f95c18f9-2d73-4d6c-a44a-bb3f35a79517", :topic "topic1", :owner-account {:id #uuid "f95c18f9-2d73-4d6c-a44a-bb3f35a79571", :name "user1"}, :produced-at #inst "1970-01-01T00:00:00.000-00:00", :consumed-at #inst "1970-01-01T00:00:00.000-00:00", :user "user1"})
+
 (defcard-rg show-movement
   [:div
    [:h1 [:i "Initial state"]]
-   [show-movement* {:id #uuid "f95c18f9-2d73-4d6c-a44a-bb3f35a79517", :topic "topic1", :owner-account {:id #uuid "f95c18f9-2d73-4d6c-a44a-bb3f35a79571", :name "user1"}, :produced-at #inst "1970-01-01T00:00:00.000-00:00", :consumed-at #inst "1970-01-01T00:00:00.000-00:00", :user "user1"}]])
+   [show-movement* movement]])
 
 (defcard-rg generated-show-movement
   (let [models (gen/sample (s/gen ::spec/movement))]
