0cf0cc4675bf0ca10f2feb6605110b6b4cb4afdf
Mylab <labs@onmycrowd.com>
2020-04-07 08:37:40 -0700

    Starting to work in models

---
+++ test/models/accounts/accounts_test.cljs
@@ -0,0 +1,7 @@
+(ns models.accounts.accounts-test
+  (:require [re-frame.core :as re-frame]
+            [day8.re-frame.test :refer [run-test-sync]]
+            [cljs.test :refer [use-fixtures] :refer-macros [testing is]]
+            [devcards.core :refer-macros [deftest]]
+            [clojure.spec.alpha :as s]))
+
